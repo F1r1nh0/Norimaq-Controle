9e2a40be3e8d274a118266b70bbd57de53199ab8
Erik Sousa <Eriksousa.tec@gmail.com>
2025-09-10 09:04:07 -0300

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ import { createClient } from "@supabase/supabase-js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
